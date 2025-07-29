500ec608fe1aad3b3bb6ba91d22ca90ef0324028
Bertnardo Mario Uskono <130955153+mariouskono@users.noreply.github.com>
2025-07-29 17:08:18 +0700

    Update modelling.py

--- kriteria2/modelling.py
+++ kriteria2/modelling.py
@@ -1,11 +1,10 @@
-# modelling.py
-
 import pandas as pd
-import mlflow
-import mlflow.sklearn
 from sklearn.ensemble import RandomForestClassifier
 from sklearn.model_selection import train_test_split
 from sklearn.metrics import accuracy_score
+import mlflow
+import mlflow.sklearn
+
 
 # Load data
 df = pd.read_csv('winequality_preprocessing/winequality_preprocessed.csv')
